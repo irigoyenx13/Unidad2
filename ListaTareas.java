87c61a414e9d4a4dae7580fecfcba6ec13b17ca0
irigoyenx13 <143623728+irigoyenx13@users.noreply.github.com>
2023-09-06 08:48:41 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListaTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1 = "Hacer tarea";
+        String tarea2 = "Lavar el coche";
+        String tarea3 = "Lavar los trastes"; 
+        String tarea4 = "Hacer de comer";
+        String tarea5 = "limpiar mi recamara";
+        
+        System.out.println( "Hola mi nombre es diego y mi madre me asigno hacer todas estas tareas;" + tarea1  +  tarea2  + tarea3  +  tarea4  +  tarea5);
+        
+    }
+    
+}
