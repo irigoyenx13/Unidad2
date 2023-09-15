4de7aaed44fb724ddf72e8ef97442fe9dd3605f0
irigoyenx13 <143623728+irigoyenx13@users.noreply.github.com>
2023-09-15 08:06:22 -0600

    Add files via upload

---
+++ AreaTriangulo.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package areatriangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //numeros enteros
+        //numeros decimales 
+         /**int altura1 = 10;
+        int base2 = 0;
+        
+        int resultado = base1 / altura2;
+        System.out.print(resultado);
+        * */
+        //int[] arr = {1,2,3};
+        //System.out.println{arr[3]};
+        
+        int alturaA = 50;
+        int baseB = 30;
+        String numeroC = "45";
+        
+        
+        
+        int multiplicacion = alturaA * baseB/2;
+        
+        
+        
+        
+        
+        System.out.println("El area del triangulo es; " + multiplicacion);
+        
+    }
+    
+}
